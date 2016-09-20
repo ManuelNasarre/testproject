4155d29c8a3029cead558349c788ca20000affe9
ManuelNasarre <manuelnasarre@hotmail.com>
2016-09-20 18:53:39 +0200

    primer commit

---
+++ PruebaActividad.java
@@ -0,0 +1,13 @@
+package com.example.alumno.prueba;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PruebaActividad extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prueba_actividad);
+    }
+}
