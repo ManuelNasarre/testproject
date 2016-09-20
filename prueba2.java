5b9f7807b383c4d1d1c528bc5b9f8e45b1a7c019
ManuelNasarre <manuelnasarre@hotmail.com>
2016-09-20 18:57:40 +0200

    segundo commit

---
+++ prueba2.java
@@ -0,0 +1,12 @@
+package com.example.alumno.prueba;
+
+/**
+ * Created by Alumno on 20/09/2016.
+ */
+public class prueba2 {
+
+    public static void main(String arg[]){
+        System.out.println("Hola Mundo");
+
+    }
+}
